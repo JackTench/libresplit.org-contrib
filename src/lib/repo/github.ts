1eb9dca4e33d4f42a4845ce2fcbc3c3ad1288877
JackTench <79285604+JackTench@users.noreply.github.com>
2026-09-14 22:23:19 +0100

    Fetch data async from GitHub API

--- src/lib/repo/github.ts
+++ src/lib/repo/github.ts
@@ -4,6 +4,12 @@ const OWNER = "LibreSpluit";
 const REPO = "LibreSplit-resources";
 const BRANCH = "main";
 
-export function fetchResources(): RepoResource[] {
-  // TODO: Fetch list of resources from the GitHub repo.
+export async function fetchResources(): Promise<RepoResource[]> {
+  const response = await fetch(
+    `https://api.github.com/repos/${OWNER}/${REPO}/git/trees/${BRANCH}?recursive=1`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch LibreSplit resources");
+  }
 }
