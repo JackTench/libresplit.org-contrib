bf93b4cc1b6c44ec2e0a4b9b66c2004c673b96bb
JackTench <79285604+JackTench@users.noreply.github.com>
2026-09-14 22:21:21 +0100

    Scaffold fetcher to get data from GitHub

---
+++ src/lib/repo/github.ts
@@ -0,0 +1,9 @@
+import type { RepoResource } from "./model";
+
+const OWNER = "LibreSpluit";
+const REPO = "LibreSplit-resources";
+const BRANCH = "main";
+
+export function fetchResources(): RepoResource[] {
+  // TODO: Fetch list of resources from the GitHub repo.
+}
