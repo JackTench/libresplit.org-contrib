0f6cd2e93ae8e40c3f7c0c5da2ffe7ef81224294
Jack Tench <79285604+JackTench@users.noreply.github.com>
2026-09-14 10:12:15 +0100

    Documentation navigation cards pop when hovered (#46)
    * Documentation navigation cards now pop when hovered
    
    * Use the same classes as installation documentation

--- src/app/docs.tsx
+++ src/app/docs.tsx
@@ -49,7 +49,7 @@ export function Docs() {
         <For each={docs}>
           {(doc) => (
             <a href={doc.to}>
-              <Card class="h-full transition hover:border-primary hover:shadow-lg">
+              <Card class="h-full transition-[border-color,box-shadow,translate,background-color] duration-200 hover:-translate-y-1 hover:border-primary/40 hover:bg-accent/30 hover:shadow-lg">
                 <CardHeader>
                   <CardTitle>{doc.title}</CardTitle>
                   <CardDescription>{doc.description}</CardDescription>
