6e62ab206b970b889fd841d3ca79b496bc325756
JackTench <79285604+JackTench@users.noreply.github.com>
2026-09-14 23:13:43 +0100

    Add suitable icon to card

--- src/app/resources/ResourceCard.tsx
+++ src/app/resources/ResourceCard.tsx
@@ -1,3 +1,5 @@
+import { FileCode, Palette, Timer } from "lucide-solid";
+
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { getDisplayName, type RepoResource } from "@/lib/resources/model";
 
@@ -7,10 +9,22 @@ interface ResourceCardProps {
 
 export function ResourceCard(props: ResourceCardProps) {
   const name = getDisplayName(props.resource);
+  const icon = () => {
+    switch (props.resource.type) {
+      case "split":
+        return Timer;
+      case "theme":
+        return Palette;
+      case "auto-splitter":
+        return FileCode;
+    }
+  };
+  const Icon = icon();
 
   return (
     <Card>
       <CardHeader>
+        <Icon />
         <CardTitle>{name}</CardTitle>
       </CardHeader>
     </Card>
